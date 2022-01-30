f493f25a41482b379b2d4cd31a09451e80722ad5
興田寅一(r202003453pl) <(r202003453pl)@KU-info2020-018>
2022-01-30 16:40:16 +0900

    version 2

--- Ex5/src/ex5/Main.java
+++ Ex5/src/ex5/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Welcome to my room Mr Kaiya");
+		System.out.println("Welcome to my room Mr reo");
 	}
 
 }
